ba5907409527108fe02c58cc75b2cfc36ad6ba1c
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-08-09 15:28:59 +0530

    Insert element at the beginning

--- Python Linked List/Single_LinkedList.py
+++ Python Linked List/Single_LinkedList.py
@@ -3,8 +3,8 @@
 
 class Node:
     def __init__(self,data):
-        self.data = data;  # n1.data = 5, n2.data = 10, n3.data = 25, n4.data = 59
-        self.next = None;  # n1.next = None, n2.next = None, n3.next = None, n4.next = None
+        self.data = data;  # n1.data = 5, n2.data = 10, n3.data = 25, n4.data = 59, nb.data = 66
+        self.next = None;  # n1.next = None, n2.next = None, n3.next = None, n4.next = None, nb.next = None
 node1 = Node(7)
 print(node1.data)
 print(node1.next)
@@ -26,6 +26,14 @@ class LinkedList:
                 print(a.data, end = " ") # a.data = n1.data
                 a = a.next               # a = n1.next
 
+    
+
+    def insert_at_beginning(self,data):  # data = 66
+        print()
+        nb = Node(data)     #nb = Node(66)
+        nb.next = self.head  # nb.next = n1
+        self.head = nb       # sll.head = nb
+
 
 n1 = Node(5)
 sll = LinkedList()
@@ -36,4 +44,8 @@ n3 = Node(25)
 n2.next = n3
 n4 = Node(59)
 n3.next = n4
+sll.traversal()
+
+# Calling the insert_at_beginning() 
+sll.insert_at_beginning(66)
 sll.traversal()
